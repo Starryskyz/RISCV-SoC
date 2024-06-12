260de6a3ae9d652c33997f62468656287f51a5b0
Starryskyz <starryskyz@qq.com>
2024-06-12 12:09:23 +0800

    update regfile

--- RegisterFile.v
+++ RegisterFile.v
@@ -17,9 +17,41 @@ module RegisterFile(
 
     
     always@(negedge clk or posedge rst) begin 
-        if(rst)
-            for( i=0; i<32; i=i+1) 
-                RegFile[i][31:0] <= 32'b0;
+        if(rst) begin
+            RegFile[0] <= 32'b0;
+            RegFile[1] <= 32'b0;
+            RegFile[2] <= 32'b0;
+            RegFile[3] <= 32'b0;
+            RegFile[4] <= 32'b0;
+            RegFile[5] <= 32'b0;
+            RegFile[6] <= 32'b0;
+            RegFile[7] <= 32'b0;
+            RegFile[8] <= 32'b0;
+            RegFile[9] <= 32'b0;
+            RegFile[10] <= 32'b0;
+            RegFile[11] <= 32'b0;
+            RegFile[12] <= 32'b0;
+            RegFile[13] <= 32'b0;
+            RegFile[14] <= 32'b0;
+            RegFile[15] <= 32'b0;
+            RegFile[16] <= 32'b0;
+            RegFile[17] <= 32'b0;
+            RegFile[18] <= 32'b0;
+            RegFile[19] <= 32'b0;
+            RegFile[20] <= 32'b0;
+            RegFile[21] <= 32'b0;
+            RegFile[22] <= 32'b0;
+            RegFile[23] <= 32'b0;
+            RegFile[24] <= 32'b0;
+            RegFile[25] <= 32'b0;
+            RegFile[26] <= 32'b0;
+            RegFile[27] <= 32'b0;
+            RegFile[28] <= 32'b0;
+            RegFile[29] <= 32'b0;
+            RegFile[30] <= 32'b0;
+            RegFile[31] <= 32'b0;
+        end
+            
         else if( (RegWriteEN3 == 1'b1) && (Address3 != 5'b0) )
             RegFile[Address3] <= RegDataW3;
         else
